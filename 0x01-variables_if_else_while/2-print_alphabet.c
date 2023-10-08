2df74a73096aa4dae19f9c33849e9d51ab4b43e7
M0imaritim <moimaritim@gmail.com>
2023-10-08 03:36:34 -0700

    alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: A program that prints all alphabeth letters in lower case
+*Return: always 0(successs)
+*/
+
+int main(void)
+{
+	char Lowercase = 'a';
+
+	while (Lowercase <= 'z')
+	{
+		putchar(Lowercase);
+		Lowercase++;
+	}
+	putchar(\n)
+
+	return (0);
+
+}
