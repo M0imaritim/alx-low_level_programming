06cf74570c6d575f4d318563a3e87c782f93534d
M0imaritim <moimaritim@gmail.com>
2023-10-08 19:43:13 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: A program that prints all base 16 digits
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	int hex = 0;
+
+	while (hex < 16)
+	{
+		if (hex < 10)
+		{
+			putchar('0' + hex);
+		}
+		else
+		{
+			putchar('A' + (hex - 10));
+		}
+		hex++;
+
+
+	}
+	putchar('\n');
+	return (0);
+}
