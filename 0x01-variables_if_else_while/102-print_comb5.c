efe222abaf74b767055dee0376e29139ed26efe3
M0imaritim <moimaritim@gmail.com>
2023-10-09 19:34:31 -0700

    print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - Entry Point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 <= 99)
+	{
+		int num2 = num1;
+
+		while (num2 <= 99)
+		{
+			if (num1 != num2)
+			{
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar(',');
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar(' ');
+			}
+			num2++;
+		}
+
+		num1++;
+	}
+
+	return (0);
+}
