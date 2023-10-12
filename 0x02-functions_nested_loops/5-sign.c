e7537fd15a5cb876af78eb4f7bc4f9acea3e8b00
M0imaritim <moimaritim@gmail.com>
2023-10-12 02:29:43 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *print_sign - determines whether a number is positive negative or zero
+ *and prints its sign
+ * @n: value of integer
+ * Return: 1 if positive, 0 if zero and -1 if negative
+ */
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (0);
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0')
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0')
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0')
+	}
+	return (0);
+}
