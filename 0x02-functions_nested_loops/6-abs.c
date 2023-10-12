3ad94fe8d179fad051fc7438738e4bf272f3720a
M0imaritim <moimaritim@gmail.com>
2023-10-12 08:04:45 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,11 @@
 /**
  * _abs - prints the absolute value of a number
  *
- * @int: parameter value
+ * @n: parameter value
  * Return: always 0(success)
  */
 int _abs(int n)
 {
-
 	if (n < 0)
 	{
 		n = -n;
