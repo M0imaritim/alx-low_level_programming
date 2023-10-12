13eb29d7eeb1769719d0623442de506b6f0a68a1
M0imaritim <moimaritim@gmail.com>
2023-10-11 18:53:19 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowe case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
