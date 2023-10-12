b6925ddb8eacf0780272f36b9fc1591f008ab60a
M0imaritim <moimaritim@gmail.com>
2023-10-12 06:48:57 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - prints the absolute value of a number
+ *
+ * @int: parameter value
+ * Return: always 0(success)
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		n = (n * -1);
+
+		_putchar('0' + n);
+	}
+	else if (n == 0)
+	{
+		n = (n * 0);
+
+		_putchar('0' + n);
+	}
+	else
+	{
+		_putchar('0' + n);
+	}
+	return (0);
+}
