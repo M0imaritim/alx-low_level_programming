a706fb8e2236c196b7aa699a49b3319c6d91a7d8
M0imaritim <moimaritim@gmail.com>
2023-10-12 19:54:34 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 to 9, excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number != '2' && number != '4')
+		{
+			_putchar(number);
+		}
+	}
+
+	_putchar('\n');
+}
