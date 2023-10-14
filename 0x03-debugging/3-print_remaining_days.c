d1401829116ee2689c8ea38a7873ebfa408b24a6
M0imaritim <moimaritim@gmail.com>
2023-10-14 10:27:31 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -21,8 +21,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
+		printf("Remaining days: %d\n", (366 - day));
 	else
 	{
 		if (month == 2 && day == 60)
@@ -33,7 +32,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			printf("Remaining days: %d\n", (365 - day));
 		}
 	}
 }
