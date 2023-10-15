236756e6c67ff1722c39b219b050a120f1010069
M0imaritim <moimaritim@gmail.com>
2023-10-15 05:59:35 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,15 +14,18 @@ int main(void)
 
 		while (num2 <= 99)
 		{
-			if (num1 != num2)
+			if (num1 != num2 && num2 <= 99)
 			{
 				putchar((num1 / 10) + '0');
 				putchar((num1 % 10) + '0');
 				putchar(' ');
 				putchar((num2 / 10) + '0');
 				putchar((num2 % 10) + '0');
-				putchar(',');
-				putchar(' ');
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			num2++;
 		}
