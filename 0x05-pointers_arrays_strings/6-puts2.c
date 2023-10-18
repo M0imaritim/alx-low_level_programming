6f315fc7a255a5a1c270033d627f1c064d0f3812
M0imaritim <moimaritim@gmail.com>
2023-10-17 17:38:40 -0700

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,35 +5,26 @@
  * puts2 - Prints every other character of a string
  * @str: The input string
  */
-/*void puts2(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
 	if (str == NULL)
-	*{
+	{
 		return;
-		}
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i += 2;
 	}
-	_putchar('\n');
-	}*/
-void puts2(char *str)
-{
-	int count = 0;
 
-	while (count >= 0)
+	while (i >= 0)
 	{
-		if (str[count] == '\0')
+		if (str[i] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
-		if (count % 2 == 0)
-			_putchar(str[count]);
-		count++;
+		if  (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 }
