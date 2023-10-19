4a14a1990f63ccf1f94b879df0719ff46b078420
M0imaritim <moimaritim@gmail.com>
2023-10-19 10:56:12 -0700

    string concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat - concatnatestwo strings
+ * @dest: the first string that the second string will be appended to
+ * @src: the second string to be appended to the first
+ * @n: the maximum characters to be appended
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int j = 0;
+	int dest_len = 0;
+	int src_len = 0;
+
+	if (dest == NULL || src == NULL)
+	{
+		return (NULL);
+	}
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while(src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	while (j < n && j < src_len)
+	{
+		dest[dest_len + j] = src[j];
+		j++;
+	}
+	dest_len += j;
+	if (src_len > n)
+	{
+		dest[dest_len + j] = '\n';
+	}
+	return (dest);
+}
