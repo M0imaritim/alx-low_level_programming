9f9259b0ef6e08e8701ed8e201e4bc4f3884152f
M0imaritim <moimaritim@gmail.com>
2023-10-19 18:24:00 -0700

    All to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - Changes all lowercase letters in a string to uppercase.
+ * @str: The input string to be modified.
+ * Return: A pointer to the modified string.
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	if (str == NULL)
+	{
+		return(NULL);
+	}
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - ('a' - 'A');
+		}
+		ptr++;
+	}
+
+	return str;
+}
