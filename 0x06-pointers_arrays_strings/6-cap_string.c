ba0cb9f972cb590aab083bfa6f5ad5d90195be51
M0imaritim <moimaritim@gmail.com>
2023-10-20 09:58:39 -0700

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words in a string.
+ * @str: The input string.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			if (ptr == str || (
+				   *(ptr - 1) == ' ' || *(ptr - 1) == '\t' ||
+				   *(ptr - 1) == '\n' ||
+				   *(ptr - 1) == ',' || *(ptr - 1) == ';' ||
+				   *(ptr - 1) == '.' || *(ptr - 1) == '!' ||
+				   *(ptr - 1) == '?' || *(ptr - 1) == '"' ||
+				   *(ptr - 1) == '(' || *(ptr - 1) == ')' ||
+				   *(ptr - 1) == '{' || *(ptr - 1) == '}'))
+			{
+			        *ptr -= 32;
+			}
+		}
+		ptr++;
+	}
+
+	return (str);
+}
