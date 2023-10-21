83ede568487e19eb8f7816db502101cd2118fb66
M0imaritim <moimaritim@gmail.com>
2023-10-20 17:28:34 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,22 +11,16 @@ char *leet(char *str)
 
 	while (*ptr)
 	{
-		char leet_replacement = 0;
-
 		if (*ptr == 'a' || *ptr == 'A')
-			leet_replacement = '4';
+			*ptr = '4';
 		else if (*ptr == 'e' || *ptr == 'E')
-			leet_replacement = '3';
+			*ptr = '3';
 		else if (*ptr == 'o' || *ptr == 'O')
-			leet_replacement = '0';
+			*ptr = '0';
 		else if (*ptr == 't' || *ptr == 'T')
-			leet_replacement = '7';
+			*ptr = '7';
 		else if (*ptr == 'l' || *ptr == 'L')
-			leet_replacement = '1';
-
-		if (leet_replacement)
-			*ptr = leet_replacement;
-
+			*ptr = '1';
 		ptr++;
 	}
 
