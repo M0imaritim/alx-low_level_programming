0c3792e0b8dce9107e7a1c173b6ae9527d5031a3
M0imaritim <moimaritim@gmail.com>
2023-10-21 02:37:56 -0700

    print combination 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,14 +5,17 @@
 *Return: Always 0 (success)
 */
 
-int main(void) {
+int main(void)
+{
 	int digit1 = 0;
 
-	while (digit1 <= 9) {
-		int digit2 = 0;
+	while (digit1 <= 9)
+	{
+		int digit2 = digit1 + 1;
 
-		while (digit2 <= 9) {
-			int digit3 = 0;
+		while (digit2 <= 9)
+		{
+			int digit3 = digit2 + 1;
 
 			while (digit3 <= 9)
 			{
@@ -22,7 +25,7 @@ int main(void) {
 					putchar('0' + digit2);
 					putchar('0' + digit3);
 
-					if (digit1 != 9 || digit2 != 8 || digit3 != 7)
+					if (digit1 != 7 || digit2 != 8 || digit3 != 9)
 					{
 						putchar(',');
 						putchar(' ');
