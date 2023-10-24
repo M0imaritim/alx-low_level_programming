bd8cb4bd1092c0764b3f80618ea4a639bf41e8d1
M0imaritim <moimaritim@gmail.com>
2023-10-24 07:34:50 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,8 +3,8 @@
 /**
  * _strpbrk - searches a string for any of a set bytes
  * @s: pointer to the byte in string be searched
- * @accept: pointer to the bytes in the string being
- * Return : pointer to bytes in s or NULL if not found
+ * @accept: pointer to the bytes of characters to search for
+ * Return: pointer to bytes in s or NULL if not found
  *
  */
 char *_strpbrk(char *s, char *accept)
