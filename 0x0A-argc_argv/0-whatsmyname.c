8ae21cbdcb0619440ce87b7c4c3c64d394737eba
M0imaritim <moimaritim@gmail.com>
2023-10-29 09:29:06 -0700

    making it betty compliant

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - Prints its name followed by a new line
+ * @argc: number of arguments passed to the function
+ * @argv: actual arguments
+ * Return: Always 0(success)
+ */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
