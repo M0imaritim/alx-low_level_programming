e8a7221c249ba8ec20d195e0cfa6cf0b0cb27fa8
M0imaritim <moimaritim@gmail.com>
2023-10-30 07:52:20 -0700

    add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,19 +16,22 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
+		int j = 0;
+
 		if (argc <= 1)
 		{
-			printf("0");
+			printf("0\n");
 		}
-		if (!isdigit(*argv[i]))
+
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
