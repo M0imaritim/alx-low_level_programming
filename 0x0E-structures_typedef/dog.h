019b09c880cf5baf73fc5981a67695d1851113f4
M0imaritim <moimaritim@gmail.com>
2023-11-06 11:29:25 -0800

    new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,14 +10,13 @@
  * @owner: dog's owner
  * Description: a simple structure to define the attributes of a dog
  */
-typedef struct dog dog_t;
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
