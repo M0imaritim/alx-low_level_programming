e7ede5627d7d26c4c4fada870337973baf914636
M0imaritim <moimaritim@gmail.com>
2023-11-08 05:02:12 -0800

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to function that prints name
- *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
