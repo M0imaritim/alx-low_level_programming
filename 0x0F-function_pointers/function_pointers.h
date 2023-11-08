7e580572a9ac9d789c73588ea88289bf7f9490d1
M0imaritim <moimaritim@gmail.com>
2023-11-08 07:40:51 -0800

    array iteration

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,9 +2,7 @@
 #define FUNC_POINTER_H
 
 void print_name(char *name, void (*f)(char *));
-/**
- * void array_iterator(int *array, size_t size, void (*action)(int));
- */
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 
