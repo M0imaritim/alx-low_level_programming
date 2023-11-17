ac777e558fd5af29a486f883bd032d2885fdc82b
M0imaritim <moimaritim@gmail.com>
2023-11-17 08:22:52 -0800

    betty fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,45 +12,26 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *current;
 
-	if (head == NULL || str == NULL)
+	if (!head || !str)
 	{
 		return (NULL);
 	}
-
 	new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (!new_node)
 	{
 		return (NULL);
 	}
-
 	new_node->str = strdup(str);
 
-	if (new_node->str == NULL)
+	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->len = strlen(str);
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
-	else
-	{
-		current = *head;
-
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node;
-	}
-
+	new_node->next = *head;
+	*head = new_node;
 	return (new_node);
 }
