c16e1d72c73167aaadc558b82b58dae8d0d381eb
M0imaritim <moimaritim@gmail.com>
2023-11-22 18:19:32 -0800

    some error fixes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
-	if (temp == NULL || temp->next == NULL)
+	if (temp == NULL)
 	{
 		free(new_node);
 		return (NULL);
