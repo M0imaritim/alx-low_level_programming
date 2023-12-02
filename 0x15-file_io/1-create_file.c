17176d2e70bdf9efe9de6ab7998ab109a87536de
M0imaritim <moimaritim@gmail.com>
2023-12-01 17:09:40 -0800

    error fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,14 +23,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		text_content = "";
-	if (text_content != NULL)
+	written = write(fildes, text_content, strlen(text_content));
+	if (written == -1)
 	{
-		written = write(fildes, text_content, strlen(text_content));
-		if (written == -1)
-		{
-			close(fildes);
-			return (-1);
-		}
+		close(fildes);
+		return (-1);
 	}
 	close(fildes);
 	return (1);
