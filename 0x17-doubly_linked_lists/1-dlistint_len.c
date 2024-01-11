02488ecc40bbb0997171c58e4b16ed359365b2b5
m0imaritim <moimaritim@gmail.com>
2024-01-11 06:48:16 -0800

    list length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: pointer to the hesd of list
+ * Return: number of elementsin list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
