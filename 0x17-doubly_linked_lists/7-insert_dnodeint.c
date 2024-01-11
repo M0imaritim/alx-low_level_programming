3848a53188685dfc9aec0273fc1567c3d8a7bb1e
m0imaritim <moimaritim@gmail.com>
2024-01-11 13:01:47 -0800

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,6 +10,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new, *tmp = *h;
 	unsigned int i = 0;
+
 	if (!h)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
